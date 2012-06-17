7cfb32c9950bb621e93df16ed780b4a131e8db2c
Matthew Stickney <fiendishlinux@gmail.com>
2012-06-16 23:29:43 -0400

    Add initial docbuild code

---
+++ docbuild/docbuild.py
@@ -0,0 +1,77 @@
+from flask import Flask, send_from_directory, request, redirect, url_for, g
+from werkzeug import secure_filename
+from jinja2 import Template
+import tempfile, os, shutil, subprocess
+from zipfile import ZipFile
+
+app = Flask(__name__)
+
+# configuration
+EMACS_CMD=['emacs', '--batch', '--visit={{file}}', '--funcall', 'org-export-as-pdf']
+CONV_DIR='conversion'
+LOG_FILE='{{file}}.log'
+
+def mk_dir():
+	tmpdir = tempfile.mkdtemp()
+	path = os.path.join(tmpdir, CONV_DIR)
+	os.mkdir(path)
+	return tmpdir
+
+def pdf_name(filename):
+	return '.'.join([filename.rsplit('.', 1)[0], 'pdf'])
+
+def package_files(tmpdir, filename):
+	logfile = Template(LOG_FILE).render(file=filename)
+	pdffile = pdf_name(filename)
+	shutil.copy(os.path.join(tmpdir, logfile), os.path.join(tmpdir, CONV_DIR))
+	shutil.copy(os.path.join(tmpdir, pdffile), os.path.join(tmpdir, CONV_DIR))
+	with ZipFile(os.path.join(tmpdir, 'org.zip'), 'a') as zip:
+		zip.write(os.path.join(tmpdir, CONV_DIR), arcname=CONV_DIR)
+	return send_from_directory(tmpdir, 'org.zip')
+
+def build_org_file(tmpdir, filename):
+	filepath = os.path.join(tmpdir, filename)
+	logfile = Template(LOG_FILE).render(file=filepath)
+	logpath = os.path.join(tmpdir, logfile)
+	cmd = [Template(a).render(file=filepath) for a in EMACS_CMD]
+	with open(logpath, 'w') as logf:
+		try:
+			subprocess.check_call(cmd, stderr=logf)
+		except CalledProcessError:
+			return abort(400)
+		return package_files(tmpdir, filename)	
+
+@app.route('/build/', methods=['GET', 'POST'])
+def return_image():
+	if request.method == 'POST':
+		file = request.files['file1']
+		if file:
+			filename=secure_filename(file.filename)
+			dir = mk_dir()
+			path = os.path.join(dir, secure_filename(filename))
+			file.save(path)
+			return build_org_file(dir, secure_filename(filename))
+
+	return '''
+	<!doctype html>
+	<html>
+	<head>
+	<title>Upload File</title>
+	</head>
+	<body>
+	<h1>Upload new file</h1>
+	<form action="" method="post" enctype="multipart/form-data">
+	<p><input type="file" name="file1"></p>
+	<input type="Submit" value="Upload">
+	</form>
+	</body>
+	</html>
+'''
+
+@app.route('/display/')
+def display_filename():
+	return request.args['filename']
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
